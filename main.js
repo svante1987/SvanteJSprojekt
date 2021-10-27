a7fb85b43ff0f27b4f7ff2de3cdc42c504405e1e
Svante Pålsson <svantepalsson1987@gmail.com>
2021-10-27 21:10:58 +0200

    Nu ändrar priset färg beroende på om det är oförändrat, plus eller minus. Mer kommentarer om detta i koden.

--- main.js
+++ main.js
@@ -2,9 +2,16 @@
 // wss står för websocket secure connection
 let webSocket = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
 let stockPriceElement = document.getElementById('stockPrice');
+let lastPrice = null;
 
 webSocket.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
+    let price = parseFloat(stockObject.p).toFixed(2);
+    stockPriceElement.innerText = price;
+/*Om lastPrice är null eller oförändrat då blir siffrorna svarta. Price större än lastPrice då blir den grön, ifall mindre röd.
+! = not / inte          === = strictly equal too/ strikt likamed
+|| = or / eller         ? = if/om       : = delimiter / avgränsare*/                       
+    stockPriceElement.style.color = !lastPrice || lastPrice === price ? 'black' : price > lastPrice ? 'green' : 'red';
 
-    stockPriceElement.innerText = parseFloat (stockObject.p).toFixed(2);
+    lastPrice = price;
 };
