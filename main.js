8ff5172008e8bef45a486e0447bf19839fb0526a
Svante Pålsson <svantepalsson1987@gmail.com>
2021-10-27 20:40:35 +0200

    Jag skriver nu ut priset på ethereum på min websida genom HTMLElement interfacet innerText

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // min websocket som hämtar information om kryptovaluta från binance api url.
 // wss står för websocket secure connection
 let webSocket = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
-
+let stockPriceElement = document.getElementById('stockPrice');
 
 webSocket.onmessage = (event) => {
     let stockObject = JSON.parse(event.data);
 
-    console.log(stockObject.p);
+    stockPriceElement.innerText = stockObject.p;
 };
