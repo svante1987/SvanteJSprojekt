50b5a318cbbea9d30becaa152b0cecc3b3c7d951
Svante Pålsson <svantepalsson1987@gmail.com>
2021-10-27 20:33:04 +0200

    JSON.parse för att göra ett json objekt utav json info jag hämtar.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+// min websocket som hämtar information om kryptovaluta från binance api url.
+// wss står för websocket secure connection
 let webSocket = new WebSocket('wss://stream.binance.com:9443/ws/etheur@trade');
 
 
 webSocket.onmessage = (event) => {
-    console.log(event.data);
-}
+    let stockObject = JSON.parse(event.data);
+
+    console.log(stockObject.p);
+};
